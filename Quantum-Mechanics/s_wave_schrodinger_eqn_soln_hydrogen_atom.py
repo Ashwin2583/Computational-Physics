3388ff2ca38af80cee7b48c157debe4f40695b0e
Ashwin2583 <esashwin04@gmail.com>
2025-10-29 11:24:41 +0530

    changes the plotting from R(r) to u(r) and renamed

--- Quantum-Mechanics/s_wave_schrodinger_eqn_soln_hydrogen_atom.py
+++ Quantum-Mechanics/s_wave_schrodinger_eqn_soln_hydrogen_atom.py
@@ -21,25 +21,41 @@ for i in range(n):
         H[i,i+1] = -coeff
 
 eigen_value, eigen_vector = np.linalg.eigh(H)
-energies = eigen_value[:3]
+energies = eigen_value[:2]
 
 l_s = ['-','--','-.']
-plt.figure()
-for i in range(3):
+
+fig = plt.figure(figsize=(18,10))
+
+ax_u = fig.add_subplot(1,2,1)
+u_2 = []
+for i in range(2):
     print(f"Energy state {i+1}: ",energies[i])
     u = eigen_vector[:,i]
     norm = np.sqrt(np.sum(u**2) * dx)
     u_normalized = u / norm
     if np.mean(u_normalized[:50]) < 0:
        u_normalized *= -1
-    R = u/r
-    if np.mean(R[:50]) < 0:
-        R *= -1
-    plt.plot(r/ao,R, label=f"state={i+1}", linestyle=l_s[i])
+    u_2.append(u_normalized**2)
+    ax_u.plot(r,u_normalized, label=f"state={i+1}", linestyle=l_s[i])
 
-plt.xlabel("r/a_o")
-plt.ylabel("R(r)")
-plt.xlim(0,16)
-plt.grid(True)
-plt.legend()
+ax_u.set_xlabel("r")
+ax_u.set_ylabel("u(r)")
+ax_u.set_xlim(0,10)
+ax_u.set_title("Eigenstates u(r)")
+ax_u.grid(True)
+ax_u.legend()
+
+ax_u2 = fig.add_subplot(1,2,2)
+for j in range(2):
+    ax_u2.plot(r,u_2[j], label=f"state={j+1}", linestyle=l_s[j])
+
+ax_u2.set_xlabel("r")
+ax_u2.set_ylabel("u(r)**2")
+ax_u2.set_xlim(0,8)
+ax_u2.set_title("Probability density P(r)")
+ax_u2.grid(True)
+ax_u2.legend()
+
+plt.tight_layout()
 plt.show()
